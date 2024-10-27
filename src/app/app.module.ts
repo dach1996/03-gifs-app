1f2fb79b2d5e598843e77396e4a88eac8e58c8b3
Danny Cardenas <danny.cardenas@bayteq.com>
2024-10-27 16:31:16 -0500

    cap 95

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GifsModule } from './gifs/gifs.module';
 import { SharedModule } from './shared/shared.module';
+import { provideHttpClient } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -16,7 +16,7 @@ import { SharedModule } from './shared/shared.module';
     GifsModule,
     SharedModule
   ],
-  providers: [],
+  providers: [provideHttpClient()],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,7 +1,14 @@
+import { HttpClient, HttpParams } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-const GIPHY_KEY = 'U4kz5GyESiiUwztMgBFXRZvI40MdBrnS';
+
 @Injectable({ providedIn: 'root' })
 export class GifsService {
+
+    private GIPHY_KEY: string = 'U4kz5GyESiiUwztMgBFXRZvI40MdBrnS';
+    private BASE_URL: string = 'https://api.giphy.com/v1/gifs/search';
+    constructor(private httpClient: HttpClient) {
+
+    }
 
     private _tagHistory: string[] = [];
 
@@ -10,7 +17,21 @@ export class GifsService {
     }
 
     searchTag(tag: string): void {
+
+
+
         this.organizeHistory(tag);
+
+        var params: HttpParams = new HttpParams().
+            set('q', tag)
+            .set('limit', '10')
+            .set('api_key', this.GIPHY_KEY);
+        this.httpClient.get(this.BASE_URL, { params })
+            .subscribe(
+                res => {
+                    console.log(res);
+                }
+            )
     }
 
     organizeHistory(tag: string): void {
