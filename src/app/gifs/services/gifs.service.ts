948343b7c269cd80d0ec19f1d4f0e8188ef492aa
Danny Cardenas <danny.cardenas@bayteq.com>
2024-10-27 15:58:43 -0500

    cap 93

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -3,14 +3,23 @@ import { Injectable } from "@angular/core";
 @Injectable({ providedIn: 'root' })
 export class GifsService {
 
-    private readonly _tagHistory: string[] = [];
+    private _tagHistory: string[] = [];
 
     get tagHistory(): string[] {
         return [...this._tagHistory];
     }
 
     searchTag(tag: string): void {
-        this._tagHistory.unshift(tag);
-        console.log(this._tagHistory);
+        this.organizeHistory(tag);
     }
+
+    organizeHistory(tag: string): void {
+        tag = tag.toLowerCase();
+        if (this._tagHistory.includes(tag)) {
+            this._tagHistory = this._tagHistory.filter(where => where !== tag);
+        }
+        this._tagHistory.unshift(tag);
+        this._tagHistory = this.tagHistory.splice(0, 10);
+    }
+
 }
--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -16,4 +16,5 @@ export class SidebarComponent {
     return this.gifsService.tagHistory;
   }
 
+
 }
