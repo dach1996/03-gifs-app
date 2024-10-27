e176cc98696d7125630654d31204ba97bb5d8e82
Danny Cardenas <danny.cardenas@bayteq.com>
2024-10-27 11:07:53 -0500

    cap 91

--- src/app/gifs/components/search-box/search-box.component.ts
+++ src/app/gifs/components/search-box/search-box.component.ts
@@ -1,9 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'gifs-search-box',
   templateUrl: './search-box.component.html',
 })
 export class SearchBoxComponent {
+
+  @ViewChild("txtTagInput")
+  public tagInput?: ElementRef<HTMLInputElement>;
+
+  searchTag() {
+    console.log(this.tagInput?.nativeElement.value);
+  }
 
 }
