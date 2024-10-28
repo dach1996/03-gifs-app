baa434878bab13e146252079181100ab6d894b22
Danny Cárdenas <danny.cardenas@bayteq.com>
2024-10-28 01:02:16 -0500

    cap 109

---
+++ src/app/shared/components/lazy-image/lazy-image.component.ts
@@ -0,0 +1,25 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'shared-lazy-image',
+  templateUrl: './lazy-image.component.html',
+})
+export class LazyImageComponent implements OnInit {
+  ngOnInit(): void {
+    if (!this.url)
+      throw new Error('No llegó Url');
+
+  }
+  @Input()
+  public url!: string;
+
+  @Input()
+  public alt: string = '';
+
+  public hasLoaded: boolean = false;
+
+  onload() {
+    this.hasLoaded = true;
+  }
+
+}
